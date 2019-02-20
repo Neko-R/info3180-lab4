e0a8e5cd28b0337fbb2a8f92f8c543abf9760ed6
Niikou <reidneko@gmail.com>
2019-02-20 00:25:29 -0500

    setup upload folder and upload form

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from .forms import UploadForm
 
 
 ###
@@ -28,19 +29,23 @@ def about():
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
+
     if not session.get('logged_in'):
         abort(401)
 
     # Instantiate your form class
-
+    myform = UploadForm()
     # Validate file upload on submit
     if request.method == 'POST':
         # Get file data and save to your uploads folder
-
-        flash('File Saved', 'success')
+        if myform.validate_on_submit():
+            f = myform.photo.data
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File Saved', 'success')
         return redirect(url_for('home'))
 
-    return render_template('upload.html')
+    return render_template('upload.html', form=myform)
 
 
 @app.route('/login', methods=['POST', 'GET'])
